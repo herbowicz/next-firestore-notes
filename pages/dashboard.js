9963c40bf3f95a4642eae5ea3baf9c9894373e0c
herbowicz <herbowicz@gmail.com>
2023-03-27 21:15:06 +0200

    user info from context

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useAuth } from '../context/authContext'
 
 const Dashboard = () => {
-    return <div>This route is protected</div>
+    const { user } = useAuth()
+    
+    return (
+        <>
+            <p>This route is protected</p>
+            <h5>You are logged in as: {user.email}</h5>
+            <p>Your ID is: {user.uid}</p>
+        </>
+    )
 }
 
 export default Dashboard
