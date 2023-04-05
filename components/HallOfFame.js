5bc72210216196c233d5166203b27c73b32c2a07
herbowicz <herbowicz@gmail.com>
2023-04-05 07:43:09 +0200

    link

--- components/HallOfFame.js
+++ components/HallOfFame.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Table, Image } from 'react-bootstrap'
+import Link from 'next/link'
 import { collection, onSnapshot } from 'firebase/firestore'
 import { database } from '../firebase'
 
@@ -30,15 +31,17 @@ const HallOfFame = () => {
 				{users
 					.sort((a, b) => (a.points < b.points) ? 1 : -1)
 					.map((el, i) => (
-						<tr key={i}>
-							<td>{i + 1}</td>
-							<td>
-								<Image src={el.photoURL} width='22' height='22' alt='' />{' '}
-								{el.displayName || el.email}
-							</td>
-							<td>{el.role || 'user'}</td>
-							<td>{el.points}</td>
-						</tr>
+						<Link key={i} href={`/u/${el?.displayName || el.email}`} passHref>
+							<tr>
+								<td>{i + 1}</td>
+								<td>
+									<Image src={el.photoURL} width='22' height='22' alt='' />{' '}
+									{el.displayName || el.email}
+								</td>
+								<td>{el.role || 'user'}</td>
+								<td>{el.points}</td>
+							</tr>
+						</Link>
 					))}
 			</tbody>
 		</Table>
