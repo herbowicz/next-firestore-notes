1a683bd893be67ef0c632935289c1b2dd23e761b
herbowicz <herbowicz@gmail.com>
2023-04-06 12:54:22 +0200

    numbers on wheel

--- WheelOfFortune/Wheel.js
+++ WheelOfFortune/Wheel.js
@@ -13,17 +13,17 @@ const Wheel = () => {
     }
 
     return (
-        <Container className={styles.container} style={{transform: `rotate(${value}deg)`}}>
+        <Container className={styles.container}>
             <div className={styles.spin__btn} onClick={spin}>Spin</div>
-            <div className={styles.wheel}>
-            <div className={styles.number} style={{'--i': 1, '--clr': '#db7093'}}><span>Winx Club</span></div>
-            <div className={styles.number} style={{'--i': 2, '--clr': '#20b2aa'}}><span>Smeshariki</span></div>
-            <div className={styles.number} style={{'--i': 3, '--clr': '#8dc242'}}><span>Amphibia</span></div>
-            <div className={styles.number} style={{'--i': 4, '--clr': '#daa520'}}><span>The Owl House</span></div>
-            <div className={styles.number} style={{'--i': 5, '--clr': '#ff340f'}}><span>Tom and Jerry</span></div>
-            <div className={styles.number} style={{'--i': 6, '--clr': '#f0e33f'}}><span>SpongeBob</span></div>
-            <div className={styles.number} style={{'--i': 7, '--clr': '#3cb371'}}><span>Gravity Falls</span></div>
-            <div className={styles.number} style={{'--i': 8, '--clr': '#4169e1'}}><span>X-Men</span></div>
+            <div className={styles.wheel} style={{transform: `rotate(${value}deg)`}}>
+            <div className={styles.number} style={{'--i': 1, '--clr': '#db7093'}}><span>0</span></div>
+            <div className={styles.number} style={{'--i': 2, '--clr': '#20b2aa'}}><span>100</span></div>
+            <div className={styles.number} style={{'--i': 3, '--clr': '#8dc242'}}><span>2</span></div>
+            <div className={styles.number} style={{'--i': 4, '--clr': '#daa520'}}><span>10</span></div>
+            <div className={styles.number} style={{'--i': 5, '--clr': '#ff340f'}}><span>5</span></div>
+            <div className={styles.number} style={{'--i': 6, '--clr': '#f0e33f'}}><span>50</span></div>
+            <div className={styles.number} style={{'--i': 7, '--clr': '#3cb371'}}><span>1</span></div>
+            <div className={styles.number} style={{'--i': 8, '--clr': '#4169e1'}}><span>20</span></div>
         </div>
         </Container>
     )
