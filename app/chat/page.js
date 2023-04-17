bd32f3813ff79cfbba686f7d0928c047c0911ddb
herbowicz <herbowicz@gmail.com>
2023-04-17 08:02:41 +0200

    add font  for avatar

--- app/chat/page.js
+++ app/chat/page.js
@@ -1,18 +1,13 @@
 
 import supabase from '../../utils/supabase'
 import RealtimePosts from './realtime-posts'
-import Message from '../../components/Message'
 
 export const revalidate = 0
 
 export default async function Posts() {
-    const { data } = await supabase.from('posts').select()
+    const { data } = await supabase.from('posts').select().order('created_at', { ascending: true })
 
-    return <div>
-
+    return (
         <RealtimePosts serverPosts={data ?? []} />
-
-        {/* <Message /> */}
-
-    </div>
+    )
 }
