f5f0c924a5ad6eacf72dab8e83dec4de142ba37a
herbowicz <herbowicz@gmail.com>
2023-10-25 09:21:03 +0200

    fix dash

--- components/Navbar.js
+++ components/Navbar.js
@@ -88,44 +88,45 @@ const NavbarComp = () => {
                                 <Nav.Link>Dashboard</Nav.Link>
                             </Link> 
                             {user ? (
-                            <>
-                                <Link href="/chat" passHref legacyBehavior>
-                                    <Nav.Link>Chat</Nav.Link>
-                                </Link>
-                                <Link href="/shop" passHref legacyBehavior>
-                                    <Nav.Link>Shop</Nav.Link>
-                                </Link>
-                                <Link href="/records" passHref legacyBehavior>
-                                    <Nav.Link>Records</Nav.Link>
-                                </Link>
-                                <Link href="/wheel" passHref legacyBehavior>
-                                    <Nav.Link>Wheel</Nav.Link>
-                                </Link>
-                                <Link href="/documents" passHref legacyBehavior>
-                                    <Nav.Link>Docs</Nav.Link>
-                                </Link>
-                                <Link href="/edit" passHref legacyBehavior>
-                                    <Nav.Link>Edit</Nav.Link>
-                                </Link>
-                                <Nav.Link
-                                    onClick={() => {
-                                        logout()
-                                        router.push('/login')
-                                    }}
-                                >
-                                    Logout
-                                </Nav.Link>
-                            </>
-                        ) : (
-                            <>
-                                <Link href="/signup" passHref legacyBehavior>
-                                    <Nav.Link>Signup</Nav.Link>
-                                </Link>
-                                <Link href="/login" passHref legacyBehavior>
-                                    <Nav.Link>Login</Nav.Link>
-                                </Link>
-                            </>
-                        )}
+                                <>
+                                    <Link href="/chat" passHref legacyBehavior>
+                                        <Nav.Link>Chat</Nav.Link>
+                                    </Link>
+                                    <Link href="/shop" passHref legacyBehavior>
+                                        <Nav.Link>Shop</Nav.Link>
+                                    </Link>
+                                    <Link href="/records" passHref legacyBehavior>
+                                        <Nav.Link>Records</Nav.Link>
+                                    </Link>
+                                    <Link href="/wheel" passHref legacyBehavior>
+                                        <Nav.Link>Wheel</Nav.Link>
+                                    </Link>
+                                    <Link href="/documents" passHref legacyBehavior>
+                                        <Nav.Link>Docs</Nav.Link>
+                                    </Link>
+                                    <Link href="/edit" passHref legacyBehavior>
+                                        <Nav.Link>Edit</Nav.Link>
+                                    </Link>
+                                    <Nav.Link
+                                        onClick={() => {
+                                            logout()
+                                            router.push('/login')
+                                        }}
+                                    >
+                                        Logout
+                                    </Nav.Link>
+                                </>
+                            ) : (
+                                <>
+                                    <Link href="/signup" passHref legacyBehavior>
+                                        <Nav.Link>Signup</Nav.Link>
+                                    </Link>
+                                    <Link href="/login" passHref legacyBehavior>
+                                        <Nav.Link>Login</Nav.Link>
+                                    </Link>
+                                </>
+                            )}
+                        </>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
